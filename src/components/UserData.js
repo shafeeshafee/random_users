c6adc72a3d469e885f75424845d23cd49f485126
Shafee <shafeelinks@gmail.com>
2021-01-27 17:31:09 -0500

    took quote generator to the user data since its still user data

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,17 +1,31 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import User from "./User";
-import QuoteData from "./QuoteData";
 import "./UserData.css";
 
 export default function UserData() {
 	const [userData, setUserData] = useState([]);
 	const [showMore, setShowMore] = useState(false);
+	const [motto, setMotto] = useState([]);
 
 	const getRandomUserData = async () => {
 		const response = await axios.get("https://api.randomuser.me");
 		setUserData(response.data.results);
 	};
+
+	const getRandomQuote = async () => {
+		const response = await axios.get("https://freequote.herokuapp.com/");
+		setMotto(response.data);
+	};
+
+	const callDataFunctions = () => {
+		getRandomUserData();
+		getRandomQuote();
+	};
+
+	useEffect(() => {
+		getRandomQuote();
+	}, []);
 
 	useEffect(() => {
 		getRandomUserData();
@@ -19,14 +33,18 @@ export default function UserData() {
 
 	return (
 		<div className="card">
-			<button className="generate-user" onClick={getRandomUserData}>
+			<button className="generate-user" onClick={callDataFunctions}>
 				Generate a User
 			</button>
 			{userData.map((user, index) => {
 				return (
 					<div>
 						<User {...user} key={index} />
-						<QuoteData />
+						<p>
+							<span class="motto-p">
+								Motto: "<em>{motto.quote}</em>"
+							</span>
+						</p>
 					</div>
 				);
 			})}
